913d50a7b80edd4653cc41d1a7a4061c3748dbce
rajkiran <raj7kiran@gmail.com>
2022-03-15 22:51:23 +0530

    added old header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -59,7 +59,7 @@ import { Container } from 'react-bootstrap'
 const App = () => {
   return (
     <Router>
-      <NewHeader />
+      <Header />
         <main className='py-3>'>
         <Container>
           <Routes>
